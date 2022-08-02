ffe4cf7cb8964d14179c237381d313d3dd86a720
Nishant <nishantdixitias@gmail.com>
2022-08-02 13:53:56 +0530

    Updates

---
+++ precache-manifest.656637f7f81938c868f91acdc8a57e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bebcaa08062a9fedce8",
+    "url": "/my-app/static/css/main.3a41ce0f.chunk.css"
+  },
+  {
+    "revision": "7bebcaa08062a9fedce8",
+    "url": "/my-app/static/js/main.7bebcaa0.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/my-app/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/my-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "03f09d9637ca08bef699",
+    "url": "/my-app/static/js/runtime~main.03f09d96.js"
+  },
+  {
+    "revision": "918e4295ab760e677a60535f8890eb6f",
+    "url": "/my-app/index.html"
+  }
+];
